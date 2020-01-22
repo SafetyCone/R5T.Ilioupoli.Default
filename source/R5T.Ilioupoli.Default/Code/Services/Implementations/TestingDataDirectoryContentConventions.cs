217e023d4faa3b20baf5a957025516b7db3b2811
MinexAutomation <minex.automation@gmail.com>
2020-01-22 13:18:50 -0800

    Added default TestingDataDirectoryContentConventions implementation of the ITestingDataDirectoryContentConventions service.

---
+++ source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Ilioupoli.Default
+{
+    public class TestingDataDirectoryContentConventions : ITestingDataDirectoryContentConventions
+    {
+        public const string BasicTextFileNameValue = "Basic Text File.txt";
+
+
+        public string BasicTextFileName => TestingDataDirectoryContentConventions.BasicTextFileNameValue;
+    }
+}
