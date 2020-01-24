3b29c2d3008dd5de653b86db0b9d53256c2a989c
MinexAutomation <minex.automation@gmail.com>
2020-01-23 16:23:38 -0800

    Added ExampleVisualStudioSolutionFileName.

--- source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
+++ source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
@@ -7,7 +7,11 @@ namespace R5T.Ilioupoli.Default
     {
         public const string BasicTextFileNameValue = "Basic Text File.txt";
 
+        public const string ExampleVisualStudioSolutionFileNameValue = "R5T.Canterbury.Standard.sln";
+
 
         public string BasicTextFileName => TestingDataDirectoryContentConventions.BasicTextFileNameValue;
+
+        public string ExampleVisualStudioSolutionFileName => TestingDataDirectoryContentConventions.ExampleVisualStudioSolutionFileNameValue;
     }
 }
