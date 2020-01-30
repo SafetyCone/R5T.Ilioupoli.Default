ea7f6a6361651f0a7a39ceb88b2f2135e8a49691
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:27:20 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Ilioupoli.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Ilioupoli.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="TestingDataDirectoryContentConventions"/> implementation of <see cref="ITestingDataDirectoryContentConventions"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultTestingDataDirectoryContentConventions(this IServiceCollection services)
+        {
+            services.AddSingleton<ITestingDataDirectoryContentConventions, TestingDataDirectoryContentConventions>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="TestingDataDirectoryContentConventions"/> implementation of <see cref="ITestingDataDirectoryContentConventions"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryContentConventions> AddDefaultTestingDataDirectoryContentConventionsAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryContentConventions>(() => services.AddDefaultTestingDataDirectoryContentConventions());
+            return serviceAction;
+        }
+    }
+}
