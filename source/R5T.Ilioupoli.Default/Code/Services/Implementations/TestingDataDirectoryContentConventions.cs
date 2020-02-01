42de48595f200f080e2c06d5e273fcb113109192
MinexAutomation <minex.automation@gmail.com>
2020-01-31 22:56:46 -0800

    Implemented new VS2017 solution file name.

--- source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
+++ source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
@@ -8,10 +8,12 @@ namespace R5T.Ilioupoli.Default
         public const string BasicTextFileNameValue = "Basic Text File.txt";
 
         public const string ExampleVisualStudioSolutionFileNameValue = "R5T.Canterbury.Standard.sln";
+        public const string NewVisualStudio2017SolutionFileNameValue = "New Solution-VS2017.sln";
 
 
         public string BasicTextFileName => TestingDataDirectoryContentConventions.BasicTextFileNameValue;
 
         public string ExampleVisualStudioSolutionFileName => TestingDataDirectoryContentConventions.ExampleVisualStudioSolutionFileNameValue;
+        public string NewVisualStudio2017SolutionFileName => TestingDataDirectoryContentConventions.NewVisualStudio2017SolutionFileNameValue;
     }
 }
