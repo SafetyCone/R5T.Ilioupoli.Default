739b25e2edc70b75da0160acec6bc00f7784ee7d
MinexAutomation <minex.automation@gmail.com>
2020-02-01 19:42:26 -0800

    Addd VS2019 file name.

--- source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
+++ source/R5T.Ilioupoli.Default/Code/Services/Implementations/TestingDataDirectoryContentConventions.cs
@@ -9,11 +9,13 @@ namespace R5T.Ilioupoli.Default
 
         public const string ExampleVisualStudioSolutionFileNameValue = "R5T.Canterbury.Standard.sln";
         public const string NewVisualStudio2017SolutionFileNameValue = "New Solution-VS2017.sln";
+        public const string NewVisualStudio2019SolutionFileNameValue = "New Solution-VS2019.sln";
 
 
         public string BasicTextFileName => TestingDataDirectoryContentConventions.BasicTextFileNameValue;
 
         public string ExampleVisualStudioSolutionFileName => TestingDataDirectoryContentConventions.ExampleVisualStudioSolutionFileNameValue;
         public string NewVisualStudio2017SolutionFileName => TestingDataDirectoryContentConventions.NewVisualStudio2017SolutionFileNameValue;
+        public string NewVisualStudio2019SolutionFileName => TestingDataDirectoryContentConventions.NewVisualStudio2019SolutionFileNameValue;
     }
 }
